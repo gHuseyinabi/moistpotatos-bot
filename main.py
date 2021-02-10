38f5b858a99dfd27bad11e6e8c8e4fc0f741f0ee
gHuseyinabi <68753989+gHuseyinabi@users.noreply.github.com>
2021-02-10 09:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import logging
 import time
 import googlesearch
 import requests
-import json
 
 logging.basicConfig()
 logging.getLogger('discord')
 client = discord.Client()
 GreetList = ['Greetings!', 'Hello!', 'Hi!', 'Hey!']
 version = '0.92'
+
+prefix = '!'
 
 @client.event
 async def on_ready():
@@ -22,18 +23,18 @@ async def on_message(msg):
     if msg.author ==  client.user:
         return
 
-    if msg.content.startswith('!help'):
+    if msg.content.startswith(prefix + 'help'):
         print(f'{time.asctime()}: We received "!help" command!')
         await msg.channel.send(f'''Commands for version `{version}`:
-        **%help** - sends this list of commands
-        **%greet** - says hello
-        **%search** - finds and retrieves ten urls based on your search query''')
+        **{prefix}help** - sends this list of commands
+        **{prefix}greet** - says hello
+        **{prefix}search** - finds and retrieves ten urls based on your search query''')
 
-    if msg.content.startswith('!greet'):
+    elif msg.content.startswith(prefix + 'greet'):
         print(f'{time.asctime()}: We received the "%greet" command!')
         await msg.channel.send(GreetList[random.randrange(0, 4, 1)])
 
-    if msg.content.startswith('!search'):
+    elif msg.content.startswith(prefix + 'search'):
         fetchedURLS = []
         print(f'{time.asctime()}: We received the "%search" command!')
         query = msg.content[8:]
@@ -47,12 +48,10 @@ async def on_message(msg):
         for url in fetchedURLS:
             await msg.channel.send(url)
 
-    if msg.content.startswith('!reddit'):
-        hot_posts = requests.get(f'http://www.reddit.com/user/lechocolatfroid/posts.json', {'User-agent': 
+    elif msg.content.startswith(prefix + 'reddit'):
+        hot_posts = requests.get(f'http://www.reddit.com/user/lechocolatfroid/posts.json', headers={'User-agent': 
         'pc:https://github.com/moistpotato9873/moistpotatos-bot/blob/main/main.py:v0.92 (by /u/lechocolatfroid)'})
-        txt = hot_posts.text
-        print(hot_posts.text)
-        data = json.loads(txt)
+        data = hot_posts.json()
 
         
         for post in data['data']['children']:
